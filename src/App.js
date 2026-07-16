ce77c5d248d5869e8a1eb989d97709919358eae6
x-ender <chimdinkwo@gmail.com>
2026-07-16 11:04:15 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -852,6 +852,7 @@ const ASSET = {
   shotSocial: "/screenshot3.png",
   shotMarket: "/screenshot2.png",
   shotListing: "/screenshot1.png",
+  shotListings: "/screenshot1.png",
 };
 
 /* ------------------------------------------------------------------ */
